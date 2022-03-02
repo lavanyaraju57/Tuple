f6eead65a56f5df173c5697f7876a4ea2bb941f7
lavanyaraju57 <99262310+lavanyaraju57@users.noreply.github.com>
2022-03-02 20:41:01 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,41 @@
+# y="a",
+# print(y,type(y))
+
+# h=(3,6.8,'by',None)
+# print(type(h))
+# print(h[-1])
+
+# c=print('hi')
+# d=print(c)
+# print(c,type(c))
+
+# x=(1,)
+# print(x,type(x))
+
+# pr=('apple','reliance','asus','dell','samsung')
+# pr[1]="motorolla"
+
+# n="hello" , "world"
+# print(n,type(n))
+# print(n[2])
+
+# v=(1,4.2,'hello',[2,3],('H','D'))
+# v[0]=25
+
+"""TUPLE METHODS"""
+
+'''COUNT'''
+
+# v=(1,4.2,'hello',[2,3],('H','D'))
+# print(v.count(4.2))
+# print(v.count(8))
+
+'''INDEX'''
+
+# v=(1,4.2,'hello',[2,3],('H','D'))
+# print(v.index(4.2))
+# print(v.index(12,3))
+
+
+
+
